230a4629b00fa0d9b0f58b6dc558e864167c3021
Ben Jeffery <ben.jeffery@bdi.ox.ac.uk>
2023-09-29 00:05:38 +0100

    Refactor filters

--- workflow-dask/steps.py
+++ workflow-dask/steps.py
@@ -57,67 +57,29 @@ def load_ancestral_fasta(input, output, wildcards, config, params):
     ds.update({"variant_ancestral_allele": ancestral_states})
     sgkit.save_dataset(ds.drop_vars(set(ds.data_vars) - {"variant_ancestral_allele"}), ds_dir, mode="a")
 
-def mask_sites(input, output, wildcards, config, params):
-    from xarray import full_like
-    import numpy
+def pre_subset_filters(input, output, wildcards, config, params):
     import sgkit
+    import filters
+    from distributed import Client
 
-    ds_dir = input[0].replace(".vcf_done", "")
-    ds = sgkit.load_dataset(ds_dir)
-    chunks = ds.variant_position.chunks
+    with Client(config["scheduler_address"]) as client:
+        ds_dir = input[0].replace(".vcf_done", "")
+        ds = sgkit.load_dataset(ds_dir)
+        chunks = ds.variant_position.chunks
 
-    # Mask bad ancestral sites
-    wanted_variants = (ds['variant_ancestral_allele'] != '-') & (
-                ds['variant_ancestral_allele'] != '.') & (
-                                  ds['variant_ancestral_allele'] != 'N')
-    wanted_variants = wanted_variants.rename("variant_bad_ancestral_mask").chunk(
-        chunks).compute()
-    assert set(numpy.unique(ds['variant_ancestral_allele'][wanted_variants])) == {'A',
-                                                                                  'C',
-                                                                                  'G',
-                                                                                  'T'}
-    ds.update({"variant_bad_ancestral_mask": wanted_variants})
-    sgkit.save_dataset(ds.drop_vars(set(ds.data_vars) - {"variant_bad_ancestral_mask"}),
-                       ds_dir, mode="a")
-
-    # Mask sites where the ancestral state is not an allele
-    wanted_variants = ((ds['variant_ancestral_allele'] == ds['variant_allele'][:, 0]) |
-                       (ds['variant_ancestral_allele'] == ds['variant_allele'][:, 1]))
-    wanted_variants = wanted_variants.rename("variant_no_ancestral_allele_mask").chunk(
-        chunks).compute()
-    ds.update({"variant_no_ancestral_allele_mask": wanted_variants})
-    sgkit.save_dataset(
-        ds.drop_vars(set(ds.data_vars) - {"variant_no_ancestral_allele_mask"}), ds_dir,
-        mode="a")
-
-    # Mask duplicate sites with duplicate position
-    pos = ds['variant_position']
-    pos_shift_left = full_like(pos, -1)
-    pos_shift_left[0:-1] = pos[1:]
-    pos_shift_right = full_like(pos, -1)
-    pos_shift_right[1:] = pos[:-1]
-    wanted_variants = (pos != pos_shift_left) & (pos != pos_shift_right)
-    wanted_variants = wanted_variants.rename("variant_duplicate_position_mask").chunk(
-        chunks).compute()
-    ds.update({"variant_duplicate_position_mask": wanted_variants})
-    sgkit.save_dataset(
-        ds.drop_vars(set(ds.data_vars) - {"variant_duplicate_position_mask"}), ds_dir,
-        mode="a")
-
-    ## Create the combined mask - tsinfer now copes with missing and bad ancestral sites so don't use those masks
-    wanted_variants = ds['variant_duplicate_position_mask']
-    wanted_variants = wanted_variants.rename("variant_mask").chunk(chunks).compute()
-    ds.update({"variant_mask": wanted_variants})
-    sgkit.save_dataset(ds.drop_vars(set(ds.data_vars) - {"variant_mask"}), ds_dir,
-                       mode="a")
-
-
+        for filter_name in ['not_snps', 'bad_ancestral', 'no_ancestral_allele', 'not_biallelic', 'duplicate_position']:
+            mask = getattr(filters, filter_name)(ds)
+            # Rename to match sgkit convention
+            filter_name = f"variant_{filter_name}_mask"
+            mask = mask.rename(filter_name).chunk(chunks).compute()
+            ds.update({filter_name: mask})
+            sgkit.save_dataset(ds.drop_vars(set(ds.data_vars) - {filter_name}), ds_dir, mode="a")
 
 def subset_zarr_vcf(input, output, wildcards, config, params):
     import numpy
     import sgkit
     from pathlib import Path
-    from dask.distributed import Client
+    from distributed import Client
 
     with Client(config["scheduler_address"]) as client:
         chrom, start, end = parse_region(config['regions'][wildcards.region_name])
@@ -129,19 +91,87 @@ def subset_zarr_vcf(input, output, wildcards, config, params):
         variant_mask = numpy.logical_and(variant_position >= start, variant_position < end)
         ds = ds.sel(samples=sample_mask, variants=variant_mask)
         ds = ds.unify_chunks()
-        sgkit.save_dataset(ds, output[0].replace(".vcf_done", ""), auto_rechunk=True)
+        sgkit.save_dataset(ds, output[0].replace(".subset_done", ""), auto_rechunk=True)
         Path(str(output[0])).touch()
+
+
+def post_subset_filters(input, output, wildcards, config, params):
+    import numpy
+    import sgkit
+    import filters
+    import xarray
+    from distributed import Client
+
+    with Client(config["scheduler_address"]) as client:
+        ds_dir = input[0].replace(".subset_done", "")
+        ds = sgkit.load_dataset(ds_dir)
+
+        # Some filters need allele counts
+        ac = sgkit.count_call_alleles(ds, merge=True)['call_allele_count'].sum(
+            dim="samples").values #.values here so we can use numpy indexing below
+        sgkit.save_dataset(ds.drop_vars(set(ds.data_vars) - {"variant_allele_count"}), ds_dir, mode="a")
+
+        num_samples = ds.dims['samples'] * ds.dims['ploidy']
+
+        # Normal non-ref allele count:
+        ref_count = ac[:, 0]
+
+        # Calculate the ancestral allele index
+        allele_matches = (ds['variant_allele'] == ds['variant_ancestral_allele']).values
+        ancestral_indices = numpy.argmax(allele_matches, axis=1)
+        # Mark unknown ancestral alleles as REF - this is just for plots, not for inference
+        ancestral_indices[numpy.sum(allele_matches, axis=1) == 0] = 0
+        # Use the index to get the ancestral allele count
+        ancestral_count = ac[numpy.arange(len(ancestral_indices)), ancestral_indices]
+
+        total_ac = ac.sum(axis=1)
+        missing_count = num_samples - total_ac
+        derived_count = total_ac - missing_count - ancestral_count
+
+        for var_name in [
+            "ref_count",
+            "ancestral_count",
+            "missing_count",
+            "derived_count"
+        ]:
+            array = locals()[var_name]
+            # Convert to an xarray DataArray
+            array = xarray.DataArray(array, dims=["variants"], name=f"variant_{var_name}")
+            ds.update({f"variant_{var_name}": array})
+            sgkit.save_dataset(ds.drop_vars(set(ds.data_vars) - {f"variant_{var_name}"}), ds_dir,
+                               mode="a")
+
+        # Calculate the missing filters (note could use dask better here by combining filters into one call)
+        chunks = ds.variant_position.chunks
+        filter_config = config['filters'][wildcards.filter]
+        for filter_name, filter_kwargs in filter_config.items():
+            if filter_name not in ds.keys():
+                mask = getattr(filters, filter_name)(ds, **(filter_kwargs or {}))
+                # Rename to match sgkit convention
+                filter_name = f"variant_{filter_name}_mask"
+                mask = mask.rename(filter_name).chunk(chunks).compute()
+                ds.update({filter_name: mask})
+                sgkit.save_dataset(ds.drop_vars(set(ds.data_vars) - {filter_name}), ds_dir,
+                                   mode="a")
+
+        mask = xarray.full_like(ds['variant_position'], True, dtype=bool)
+        for filter_name in filter_config:
+            filter_name = f"variant_{filter_name}_mask"
+            mask &= ds[filter_name]
+        mask = mask.rename("variant_mask").chunk(chunks).compute()
+        ds.update({"variant_mask": mask})
+        sgkit.save_dataset(ds.drop_vars(set(ds.data_vars) - {"variant_mask"}), ds_dir,
+                                   mode="a")
 
 def zarr_stats(input, output, wildcards, config, params):
     import sgkit
     import json
-    import numpy
     import os
     from dask.distributed import Client
     import matplotlib.pyplot as plt
-    import xarray as xr
 
-    ds_dir = input[0].replace(".vcf_done", "")
+
+    ds_dir = input[0].replace("variant_mask", "")
     with Client(config["scheduler_address"]) as client:
         ds = sgkit.load_dataset(ds_dir)
         out = {}
@@ -154,10 +184,9 @@ def zarr_stats(input, output, wildcards, config, params):
         gt = (ds.call_genotype.stack(haplotype=("samples", "ploidy")))
         out['allele_counts'] = (gt > 0).sum(dim=['haplotype']).to_numpy().tolist()
         out['missing_count'] = int((gt == -1).sum())
-        out['num_sites_triallelic'] = int(((gt > 1).sum(dim=["haplotype"]) > 0).sum())
-        out['sites_bad_ancestral'] = int((~ds.variant_bad_ancestral_mask).sum())
-        out['sites_no_ancestral'] = int((~ds.variant_no_ancestral_allele_mask).sum())
-        out['sites_duplicate_pos'] = int((~ds.variant_duplicate_position_mask).sum())
+        for filter_name in config['filters'][wildcards.filter]:
+            filter_name = f"variant_{filter_name}_mask"
+            out[filter_name] = int((~ds[filter_name]).sum())
         out['sites_masked'] = int((~ds.variant_mask).sum())
         total_size = 0
         for dirpath, dirnames, filenames in os.walk(ds_dir):
@@ -168,38 +197,68 @@ def zarr_stats(input, output, wildcards, config, params):
         with open(output[0], "w") as f:
             f.write(json.dumps(out))
 
-        # Store the allele counts for later filtering
-        # Note that no missingness is assumed
-        ac = sgkit.count_call_alleles(ds, merge=False)['call_allele_count'].sum(dim="samples").values
+        counts = [("Ref allele", ds.variant_ref_count),
+                    ("Ancestral allele", ds.variant_ancestral_count),
+                    ("Derived allele", ds.variant_derived_count),
+                    ("Missing allele", ds.variant_missing_count)]
+
+
+        # Plot the allele count spectrum
+        fig = plt.figure(figsize=(10, 6))
+        ax = fig.add_subplot(111)
+
         num_samples = ds.dims['samples'] * ds.dims['ploidy']
+        for i, (label, count) in enumerate(counts):
+            ax.hist(count, bins=200, log=True, histtype='step', label=label)
+            text = ', '.join(f"{i}: {(count == i).sum().values}" for i in [0,1,2,num_samples-2,num_samples-1,num_samples])
+            ax.text(0.75, 0.95-(i*0.05), f"{label} {text}", fontsize=8, horizontalalignment='right', verticalalignment='top', transform=ax.transAxes)
+        ax.set_title(f'Raw allele count spectrum - {wildcards.subset_name}-{wildcards.region_name}-{wildcards.filter}')
+        ax.set_xlabel('Allele count')
+        ax.set_ylabel('Number of sites')
+        ax.legend(loc='upper right')
+        fig.savefig(
+            f"{config['data_dir']}/zarr_stats/{wildcards.subset_name}-{wildcards.region_name}-{wildcards.filter}/ac-raw.png")
 
-        # Normal non-ref allele count:
-        ref_ac = ac[:, 0]
+        fig = plt.figure(figsize=(10, 6))
+        ax = fig.add_subplot(111)
 
-        # Calculate the ancestral allele index
-        allele_matches = (ds['variant_allele'] == ds['variant_ancestral_allele']).values
-        ancestral_indices = numpy.argmax(allele_matches, axis=1)
-        # Mark unknown ancestral alleles as REF
-        ancestral_indices[numpy.sum(allele_matches, axis=1) == 0] = 0
+        mask = ds.variant_mask.values
+        for i, (label, count) in enumerate(counts):
+            ax.hist(count[mask], bins=200, log=True, histtype='step', label=label)
+            text = ', '.join(f"{i}: {(count[mask] == i).sum().values}" for i in [0,1,2,num_samples-2,num_samples-1,num_samples])
+            ax.text(0.75, 0.95-(i*0.05), f"{label} {text}", fontsize=8, horizontalalignment='right', verticalalignment='top', transform=ax.transAxes)
+        ax.set_title(f'Filtered allele count spectrum - {wildcards.subset_name}-{wildcards.region_name}-{wildcards.filter}')
+        ax.set_xlabel('Allele count')
+        ax.set_ylabel('Number of sites')
+        ax.legend(loc='upper right')
+        # Add a text box with the number of sites that have an allele count of 0, 1, 2, n-2, n-1, n
+        # where n is the number of samples
 
-        # Calculate the allele count for the ancestral allele
-        aa_ac = ac[numpy.arange(len(ancestral_indices)), ancestral_indices]
+        fig.savefig(
+            f"{config['data_dir']}/zarr_stats/{wildcards.subset_name}-{wildcards.region_name}-{wildcards.filter}/ac-filtered.png"
+        )
 
-        #Plot the allele count spectrum
-        plt.hist(ref_ac, bins=200, log=True, histtype='step', label="Ref allele count")
-        plt.hist(aa_ac, bins=200, log=True, histtype='step', label="Ancestral allele count")
-        plt.title('Allele count spectrum')
-        plt.xlabel('Allele count')
-        plt.ylabel('Number of sites')
-        plt.legend()
-        plt.savefig(f"{config['data_dir']}/zarr_stats/{wildcards.subset_name}-{wildcards.region_name}/ac.png")
+        # Plot site density
+        fig = plt.figure(figsize=(10, 6))
+        ax = fig.add_subplot(111)
+        ax.hist(ds.variant_position, bins=200, log=True, histtype='step', label='All Sites')
+        for filter_name in config['filters'][wildcards.filter]:
+            mask = ds[f"variant_{filter_name}_mask"].values
+            ax.hist(ds.variant_position[mask],
+                    bins=200, log=True, histtype='step', label=f"{filter_name} - {mask.sum()/ds.dims['variants']:.2f}")
 
-        # Save the ancestral allele counts to the dataset
-        aa_ac = xr.DataArray(aa_ac, dims=["variants"], name="variant_ancestral_allele_counts")
-        ds.update({"variant_ancestral_allele_counts": aa_ac})
-        sgkit.save_dataset(
-             ds.drop_vars(set(ds.data_vars) - {"variant_ancestral_allele_counts"}),
-         ds_dir, mode="a")
+        ax.hist(ds.variant_position[ds.variant_mask.values], bins=200, log=True, histtype='step',
+                label=f"variant_mask - {(ds.variant_mask.sum() / ds.dims['variants']).values:.2f}")
+        ax.set_title(f'Site density - {wildcards.subset_name}-{wildcards.region_name}-{wildcards.filter}')
+        ax.set_xlabel('Position')
+        ax.set_ylabel('Number of sites')
+        ax.legend(loc='upper right')
+        fig.savefig(
+            f"{config['data_dir']}/zarr_stats/{wildcards.subset_name}-{wildcards.region_name}-{wildcards.filter}/site-density.png"
+        )
+
+
+
 
 
 def summary_table(input, output, wildcards, config, params):
@@ -238,10 +297,6 @@ def summary_table(input, output, wildcards, config, params):
                     "ac==1": ac1,
                     "ac==2": ac2,
                     "missing_genotypes": stats['missing_count'],
-                    "num_sites_triallelic": stats['num_sites_triallelic'],
-                    "sites_bad_ancestral": stats['sites_bad_ancestral'],
-                    "sites_no_ancestral": stats['sites_no_ancestral'],
-                    "sites_duplicate_pos": stats['sites_duplicate_pos'],
                     "sites_masked": n_masked,
                     "inference_nbytes": ((
                                                      n_sites - n_masked) - ac1) * n_samples * n_ploidy,
@@ -257,22 +312,10 @@ def generate_ancestors(input, output, wildcards, config, threads):
     import logging
     import os
     from pathlib import Path
-    import sgkit
 
     logging.basicConfig(level=logging.INFO)
     data_dir = Path(config['data_dir'])
-    sample_data = tsinfer.SgkitSampleData(input[0])
-    ds = sgkit.load_dataset(input[0])
-    aa_ac = ds['variant_ancestral_allele_counts'][ds['variant_mask'].values].values
-    fixed_sites = (aa_ac == 0)
-    n_minus_1_sites = (aa_ac == 1)
-    n_minus_2_sites = (aa_ac == 2)
-    print(f"Fixed sites: {fixed_sites.sum()}")
-    print(f"n-1 sites: {n_minus_1_sites.sum()}")
-    print(f"n-2 sites: {n_minus_2_sites.sum()}")
-    filtered_sites = fixed_sites | n_minus_1_sites | n_minus_2_sites
-    print(f"Total filtered sites: {filtered_sites.sum()}")
-    filtered_positions = ds['variant_position'][ds['variant_mask'].values].values[filtered_sites]
+    sample_data = tsinfer.SgkitSampleData(input[0].replace("variant_mask", ""))
     os.makedirs(data_dir / "progress" / "generate_ancestors", exist_ok=True)
     with open(
             data_dir / "progress" / "generate_ancestors" / f"{wildcards.subset_name}-{wildcards.region_name}.log",
@@ -281,7 +324,6 @@ def generate_ancestors(input, output, wildcards, config, threads):
             sample_data,
             path=output[0],
             genotype_encoding=1,
-            exclude_positions=filtered_positions,
             num_threads=threads,
             progress_monitor=tsinfer.progress.ProgressMonitor(
                 tqdm_kwargs={'file': log_f, 'mininterval': 30})
@@ -302,7 +344,7 @@ def match_ancestors(input, output, wildcards, config, threads, slug):
     logging.basicConfig(level=logging.INFO)
     data_dir = Path(config['data_dir'])
     ancestors = tsinfer.AncestorData.load(input[0])
-    sample_data = tsinfer.SgkitSampleData(input[1])
+    sample_data = tsinfer.SgkitSampleData(input[1].replace("variant_mask",""))
     os.makedirs(data_dir / "progress" / "match_ancestors", exist_ok=True)
     os.makedirs(data_dir / "resume" / "match_ancestors", exist_ok=True)
     os.makedirs(os.path.dirname(output[0]), exist_ok=True)
@@ -358,7 +400,7 @@ def match_samples(input, output, wildcards, config, threads, slug):
     logging.basicConfig(level=logging.INFO)
     data_dir = Path(config['data_dir'])
     anc_ts = tskit.load(input[0])
-    sample_data = tsinfer.SgkitSampleData(input[1])
+    sample_data = tsinfer.SgkitSampleData(input[1].replace("variant_mask",""))
     os.makedirs(data_dir / "progress" / "match_samples", exist_ok=True)
     os.makedirs(data_dir / "resume" / "match_samples", exist_ok=True)
     os.makedirs(os.path.dirname(output[0]), exist_ok=True)
